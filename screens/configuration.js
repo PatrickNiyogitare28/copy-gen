ae9ccdcbb1a3e5c9a03fdf7eaea4926e1b99a2b2
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-07-20 13:22:36 +0200

    (ft): added configure screen

--- screens/configuration.js
+++ screens/configuration.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground,TextInput, Image,Pressable} from 'react-native';
 import {Icon} from 'react-native-elements';
+import SelectInput from 'react-native-select-input-ios'
 
 const Configuration = () => {
+  const options = [
+      { value: 0,label: 'Binding' },
+      { value: 1,label: 'Basic' },
+      { value: 2,label: 'Regular Plan' },
+    ]
   return (
       <View style={styles.container}>
           <View style={styles.header}>
@@ -21,7 +27,53 @@ const Configuration = () => {
           </ImageBackground>
           </View>
           
+          <View style={styles.formContainer}>
+               <TextInput style={styles.input} placeholder="Order name"/>
+               <TextInput style={styles.input} placeholder="Serial number"/>
+               <SelectInput style={styles.select} value={0} options={options} />
+               <SelectInput style={styles.select} value={2} options={options} />
 
+           </View>
+           <View style={styles.sortingContainer}>
+             <Text style={styles.sortingHeader}>SORTING</Text>
+             <View style={styles.docsContainer}>
+                 <View style={styles.leftCard}>
+                    <Image source={
+                        require("../assets/icons/envilop.png")
+                    }
+                    style={styles.iconImage}
+                    
+                    />
+                    <Text style={styles.cardText}>Sets</Text>
+                 </View>
+                 <View style={styles.rightCard}>
+                 <Image source={
+                        require("../assets/icons/envilop.png")
+                    } 
+                    style={styles.iconImage}
+                    
+                    />
+                    <Text style={styles.cardText}>Copies</Text>
+                 </View>
+             </View>
+           </View>
+           <View style={styles.attContainer}>
+               <Text  style={styles.sortingHeader}>ATTACHMENTS</Text>
+               <View style={styles.attCard}>
+                    <Icon
+                        name='folder'
+                        color='#4890bd' 
+                    />
+                    <Text style={styles.attText}>Browse your file</Text>
+               </View>
+           </View>
+           <View>
+           <Pressable style={styles.button} 
+            onPress = {() => navigation.navigate("configuration")} >
+               <Text style={styles.buttonText}
+               >Log in</Text>
+           </Pressable>
+           </View>
       </View>
 
   )
@@ -59,7 +111,103 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 18,
         fontWeight:'bold'
-    }
+    },
+     formContainer: {
+        marginTop: 10
+    },
+    input: {
+       borderRadius: 5,
+       backgroundColor: '#f0f0f0',
+       padding: 15,
+       fontSize: 15,
+       marginVertical: 10
+
+    },
+    select: {
+        borderRadius: 5,
+        backgroundColor: '#f0f0f0',
+        padding: 5,
+        marginVertical: 10,
+     },
+     sortingHeader: {
+      color: 'grey',
+      fontWeight:'bold'
+     },
+     sortingContainer: {
+        width: '100%',
+        paddingVertical:10
+     },
+     docsContainer: {
+        display:'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical:10
+     },
+     rightCard: {
+        width:'47%',
+        display:'flex',
+        flexDirection: 'row',
+        padding: 15,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: '#46a6fa',
+        alignItems:'center'
+
+     },
+     leftCard: {
+        width:'47%',
+        display:'flex',
+        flexDirection: 'row',
+        padding: 15,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: 'grey',
+        alignItems:'center'
+     },
+     cardText: {
+        color: 'grey',
+        fontSize: 15,
+        paddingLeft: 10
+     },
+     iconImage: {
+         width: 50,
+         height: 40
+     },
+     attContainer: {
+         marginTop: 5
+     },
+     attCard: {
+         borderRadius: 10,
+         borderStyle: 'dashed',
+         borderColor: '#46a6fa',
+         borderWidth: 2,
+         display:'flex',
+         flexDirection:'row',
+         marginTop: 20,
+         padding: 20,
+         justifyContent:'center',
+
+         
+     },
+     attText: {
+         color: '#46a6fa',
+         marginLeft: 10
+     },
+     button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        borderRadius: 4,
+        backgroundColor: '#46a6fa',
+        marginVertical: 10
+      },
+      buttonText: {
+        fontSize: 18,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+      },
     
    
 })
